38ae52b962f9ae323a06560f47bc725f0791348b
Norberto Ignacio <norbertoignaciojr@gmail.com>
2021-07-02 10:11:02 +0800

    fix map screen

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,44 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, ImageBackground, ScrollView, Image } from "react-native";
 
 import bgOne from "../img/bg-1.png";
 import homeStyle from "../styles/styleHome";
-import map from "../img/map.png"
-import mapStyle from "../styles/styleMap"
+import map from "../img/map.png";
+import mapStyle from "../styles/styleMap";
 
 const MapScreen = () => {
+    const [isShown, setIsShown] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsShown(false);
+        }, 2000);
+    }, []);
+
     return (
         <ImageBackground
             source={bgOne}
             style={homeStyle.background}
             resizeMode="cover"
         >
-            <View style={homeStyle.container}></View>
+            <View style={mapStyle.container}>
+                <View style={mapStyle.border}>
+                    <ScrollView horizontal={true} style={mapStyle.outerScroll}>
+                        <ScrollView style={mapStyle.innerScroll}>
+                            <View>
+                                <Image source={map} />
+                            </View>
+                        </ScrollView>
+                    </ScrollView>
+                    {isShown && <Text style={mapStyle.drag}>Drag to see </Text>}
+                </View>
 
-            <ScrollView
-                horizontal = {true}
-                style = {{height: "30%", width: "100%" }}
-                
-            >
-                <ScrollView>
-                    <Image
-                    source = {map}
-                    style = {mapStyle.map}
-                    />
+                <ScrollView style={mapStyle.bottom}>
+                    <Text style={mapStyle.roomsHeader}>Rooms</Text>
+
+                    <Text style={mapStyle.roomsText}>301 - Computer Lab 1</Text>
+                    <Text style={mapStyle.roomsText}>302 - Computer Lab 2</Text>
+                    <Text style={mapStyle.roomsText}>303 - Computer Lab 3</Text>
+
+                    <Text style={mapStyle.facultyHeader}>Faculty</Text>
+
+                    <Text style={mapStyle.facultyText}>
+                        311 - Dela Cruz, Cena, Gamora, Jaeger
+                    </Text>
+                    <Text style={mapStyle.facultyText}>
+                        312 - Dela Cruz, Cena, Gamora, Jaeger
+                    </Text>
                 </ScrollView>
-            </ScrollView>
-
-            <View style = {{ flex: 1 }}>
-                <Text style = {mapStyle.roomsHeader}>Rooms</Text>
-
-                <Text style = {mapStyle.roomsText}>301 - Computer Lab 1</Text>
-                <Text style = {mapStyle.roomsText}>302 - Computer Lab 2</Text>
-                <Text style = {mapStyle.roomsText}>303 - Computer Lab 3</Text>
-                
-                <Text style = {mapStyle.facultyHeader}>Faculty</Text>
-            
-                <Text style = {mapStyle.facultyText}>311 - Dela Cruz, Cena, Gamora, Jaeger</Text>
-                <Text style = {mapStyle.facultyText}>312 - Dela Cruz, Cena, Gamora, Jaeger</Text>
             </View>
         </ImageBackground>
     );
--- src/styles/styleMap.js
+++ src/styles/styleMap.js
@@ -4,27 +4,49 @@ export default StyleSheet.create({
     roomsHeader: {
         fontSize: 20,
         fontFamily: "Montserrat-Medium",
-        textAlign: "center",
         margin: 15,
     },
     roomsText: {
         fontFamily: "Montserrat-Medium",
-        textAlign: "center",
         margin: 15,
     },
     facultyHeader: {
         fontSize: 25,
         fontFamily: "Montserrat-Bold",
-        textAlign: "center",
         margin: 15,
     },
     facultyText: {
         fontFamily: "Montserrat-Bold",
-        textAlign: "center",
         margin: 15,
     },
-    map: {
-        borderColor: "orange",
+    innerScroll: {
+        height: "100%",
+    },
+    outerScroll: {
+        height: "50%",
+        width: "100%",
+    },
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    bottom: {
+        flex: 1,
+    },
+    border: {
+        borderColor: "#FC9C17",
         borderWidth: 5,
+    },
+    drag: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: [{ translateX: -50 }, { translateY: -30 }],
+        fontSize: 20,
+        textAlign: "center",
+        backgroundColor: "#D3D3D3",
+        color: "black",
+        padding: 10,
+        borderRadius: 5,
     },
 });
